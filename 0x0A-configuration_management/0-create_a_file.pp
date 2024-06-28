20ab4d3ec90c09860f8971582d707c272c3e7a53
Tapsman <tapiwanyoni140@gmail.com>
2024-06-28 14:36:34 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #The code creates a file inside the tmp
 file { '/tmp/school':
-  ensure   =>  'file',
-  mode     =>  '0744',
-  owner    =>  'www-data',
-  group    =>  'www-data',
-  content  =>  'I love Puppet',
+  ensure  =>  'file',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
+  content =>  'I love Puppet',
 }
