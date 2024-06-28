8f52412604edeb975501492ca5865445860cecfb
Tapsman <tapiwanyoni140@gmail.com>
2024-06-28 13:47:33 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-#The config creates a file using puppet
+#The code creates a file inside the tmp
 file { '/tmp/school':
+	ensure	=> 'file',
 	mode	=> '0744',
 	owner	=> 'www-data',
 	group	=> 'www-data',
-	content => 'I love Puppet'
+	content	=> 'I love Puppet',
 }
-
