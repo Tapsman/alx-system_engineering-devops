c2af83a775f8f52c274784ccd5e14b201db7ff6d
Tapsman <tapiwanyoni140@gmail.com>
2024-06-28 14:08:19 -0700

    2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
 # Creates a manifest that uses puppet to kill a process killmenow
-exec {'pkill killmenow':
-	path => '/usr/bin:/usr/sbin:/bin'
+exec { 'killmenow':
+	command	 =>  '/usr/bin/pkill killmenow',
+	provider =>  'shell',
+	returns  =>  [0, 1],
 }
 
