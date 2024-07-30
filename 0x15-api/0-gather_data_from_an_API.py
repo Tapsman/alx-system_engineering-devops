9b177e8b47276d48c2155bad51b0d0d5df0bf670
Tapsman <tapiwanyoni140@gmail.com>
2024-07-29 20:03:59 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This is a function that returns an ID information for given emplyee"""
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
