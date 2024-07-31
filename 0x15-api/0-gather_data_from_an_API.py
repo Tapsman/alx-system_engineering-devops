c723b3012643f8995e1ce43b6bdbbda4931716df
Tapsman <tapiwanyoni140@gmail.com>
2024-07-31 13:32:56 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"user_ID": sys.argv[1]}).json()
 
+    """Get the names"""
     completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee  {} is done with tasks({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
