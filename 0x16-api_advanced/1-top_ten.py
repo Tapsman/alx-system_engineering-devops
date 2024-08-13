968c2337d1c54aa319c6f58b758357fdc98f2afb
Tapsman <tapiwanyoni140@gmail.com>
2024-08-13 12:19:22 -0700

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Queries Reddit API and prints top 10 titles
+"""
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    """
+    This function the returns the top 10 provided subreddit
+    """
+    user = {'User-Agent': 'taps_man'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                      .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(none)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
