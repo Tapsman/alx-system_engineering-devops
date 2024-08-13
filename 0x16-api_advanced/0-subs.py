8a7ba95a260c66956088ae1df03afdb85767b18c
Tapsman <tapiwanyoni140@gmail.com>
2024-08-13 11:15:31 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Function returns total num of subscribers on given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "taps-app/0.0.1"}
+    headers = {"User-Agent": "Available-Resort-961"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
