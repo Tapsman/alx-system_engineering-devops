ee6e5d12c120fa5e5e23788941442ce4644db3c6
Tapsman <tapiwanyoni140@gmail.com>
2024-08-14 08:59:59 -0700

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,10 @@
 # This is a script that corrects a bad phpp to php in a file
 
 exec { 'fix-wordpress':
-  environment => ['DIR=/var/www/html/wp-settings.php',
-		  'OLD=phpp',
-		  'NEW=php'],
-  command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
-  path        => ['/usr/bin', '/bin'],
-  returns     => [0, 1]
+  	environment => ['DIR=/var/www/html/wp-settings.php',
+			'OLD=phpp',
+			'NEW=php'],
+  	command => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
+  	path	=> ['/usr/bin', '/bin'],
+	returns => [0, 1]
 }
