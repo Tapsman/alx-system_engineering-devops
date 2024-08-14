30ea1db6fa899763e79a9a64b75247e4968ca5ab
Tapsman <tapiwanyoni140@gmail.com>
2024-08-14 09:26:33 -0700

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # This is a script that corrects a bad phpp to php in a file
+# And once the issue is found, it may be fixed
 exec {'/etc/php5/apache2/php.ini':
   path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
   command => "sed -i 's/display_errors = off/display_errors = On/g' /etc/php5/apache2/php.ini",
